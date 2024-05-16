6ffe14c3e70a48eaf313cc7e77663ddb3356efdf
Simonedambra <simonedambra130102@gmail.com>
2024-05-16 15:39:06 +0200

    first commit

---
+++ snack1/js/main.js
@@ -0,0 +1,59 @@
+'use strict';
+const bikeArrayobj = [{
+    nome: 'ayax',
+    peso: 20,
+},
+{
+    nome: 'demon',
+    peso: 15.5,
+},
+{
+    nome: 'cront',
+    peso: 17.5,
+},
+{
+    nome: 'critech',
+    peso: 12.2,
+},
+{
+    nome: 'biken',
+    peso: 18.5,
+},
+{
+    nome: 'juback',
+    peso: 19.6,
+},
+{
+    nome: 'divrot',
+    peso: 14.8,
+},
+{
+    nome: 'mont',
+    peso: 21.4,
+},
+{
+    nome: 'desert',
+    peso: 15.7,
+},
+{
+    nome: 'xelot',
+    peso: 18.2,
+},]
+
+
+for (let i = 0; i < bikeArrayobj.length - 1; i++) {
+    let numeroMin = bikeArrayobj[i].peso
+    console.log(numeroMin)
+
+
+}
+
+
+
+
+// let result = bikeArrayobj.filter((bike) => {
+
+//     if (bikeArrayobj.name === 'mont') {
+//         console.log('1');
+//     }
+// })
